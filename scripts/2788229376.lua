2b96036fe2719c8603f113cb6228de8f98f101ac
solarisrbx <solaris.rbx.hub@gmail.com>
2022-07-16 17:06:23 -0500

    yas

--- scripts/2788229376.lua
+++ scripts/2788229376.lua
@@ -8,23 +8,13 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/solarisrbx/lunar/dev/util/lib.lua"))()
 local aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/solarisrbx/lunar/dev/util/aiming.lua"))()
 aiming.TeamCheck(false)
+aiming.FOV = 60
 
 local hood = {}
 hood.gui = library.new("Lunar")
 hood.settings = {
     silentAim = false
 }
-
-local __index
-__index = hookmetamethod(game, "__index", function(t, k)
-    if t:IsA("Mouse") and (k == "Hit" or k == "Target") and hood.settings.silentAim then
-        local SelectedPart = aiming.SelectedPart
-
-        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * 0.178)
-
-        return (k == "Hit" and Hit or SelectedPart)
-    end
-end)
 
 local aimPage = hood.gui:addPage("Aim", 0)
 local silentAim = aimPage:addSection("Silent Aim")
@@ -37,8 +27,19 @@ end)
 silentAim:addToggle("Show FOV Circle", true, function(val: boolean)
     aiming.ShowFOV = val
 end)
-silentAim:addSlider("FOV Size", 0, 0, 400, function(val: boolean)
-    aiming.FOV = val
-end)
+-- silentAim:addSlider("FOV Size", 0, 0, 400, function(val: boolean)
+--     aiming.FOV = val
+-- end)
 
 hood.gui:Notify("Injected", "Welcome to Lunar by Solaris. Enjoy your time in Da Hood!")
+
+local __index
+__index = hookmetamethod(game, "__index", function(t, k)
+    if t:IsA("Mouse") and (k == "Hit" or k == "Target") and hood.settings.silentAim then
+        local SelectedPart = aiming.SelectedPart
+
+        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * 0.178)
+
+        return (k == "Hit" and Hit or SelectedPart)
+    end
+end)
