a156fa88c47f5ccae3362e6ecd3bacf74bd45b7e
solarisrbx <solaris.rbx.hub@gmail.com>
2022-07-16 17:12:43 -0500

    aaa

--- scripts/2788229376.lua
+++ scripts/2788229376.lua
@@ -4,9 +4,12 @@
     Da Hood Script
     0.0.1
 ]]--
-
+    
+local Players = game:GetService("Players")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/solarisrbx/lunar/dev/util/lib.lua"))()
-local aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/solarisrbx/lunar/dev/util/aiming.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/solarisrbx/lunar/dev/util/aiming.lua"))()
+Aiming.TeamCheck(false)
+Aiming.VisibleCheck = false
 
 local hood = {}
 hood.gui = library.new("Lunar")
@@ -19,34 +22,35 @@ local silentAim = aimPage:addSection("Silent Aim")
 silentAim:addToggle("Enabled", false, function(val: boolean)
     hood.settings.silentAim = val
 end)
-silentAim:addToggle("Visible Check", true, function(val: boolean)
-    -- aiming.VisibleCheck = val
-end)
-silentAim:addToggle("Show FOV Circle", true, function(val: boolean)
-    -- aiming.ShowFOV = val
-end)
--- silentAim:addSlider("FOV Size", 0, 0, 400, function(val: boolean)
---     aiming.FOV = val
--- end)
 
 hood.gui:Notify("Injected", "Welcome to Lunar by Solaris. Enjoy your time in Da Hood!")
 
--- // Hook
+function Aiming.Check()
+    if not (Aiming.Enabled == true and Aiming.Selected ~= Players.LocalPlayer and Aiming.SelectedPart ~= nil) then
+        return false
+    end
+ 
+    local Character = Aiming.Character(Aiming.Selected)
+    local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
+    local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+ 
+    if (KOd or Grabbed) then
+        return false
+    end
+ 
+    return true
+end
+
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
-    -- // Check if it trying to get our mouse's hit or target
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target")) then
-        -- // If we can use the silent aim
-        if (aiming.Check()) then
-            -- // Vars
-            local TargetPart = aiming.SelectedPart
-
-            -- // Return modded val
-            return (k == "Hit" and TargetPart.CFrame or TargetPart)
+    if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
+        local SelectedPart = Aiming.SelectedPart
+        if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
+            local Hit = SelectedPart.CFrame + (SelectedPart.Velocity)
+ 
+            return (k == "Hit" and Hit or SelectedPart)
         end
     end
-
-    -- // Return
+ 
     return __index(t, k)
 end)
-
